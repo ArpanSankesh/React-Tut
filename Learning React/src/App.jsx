4f6fa0a8243c58364e63471522ea8772bd51438d
ArpanSankesh <arpansankesh830@gmail.com>
2024-10-12 13:13:33 +0530

    learning react

--- Learning React/src/App.jsx
+++ Learning React/src/App.jsx
@@ -1,7 +1,18 @@
 
 const App = () => {
+
+  const a = 'Arpan';
+
+  const abc = () => {
+    console.log('hello');
+    
+  }
+
   return (
-    <h1>Hello</h1>
+    <div className="w-full h-screen bg-zinc-900 p-10">
+      <h1 className="text-3xl">Hello, {a}</h1>
+      <button className="bg-blue-400 px-5 py-2 rounded-md mt-5" onClick={abc}>Change user</button>
+    </div>
   )
 }
 
