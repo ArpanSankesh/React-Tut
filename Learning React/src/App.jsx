1df044442724edb0ae969b424c81cf3e83a26730
ArpanSankesh <arpansankesh830@gmail.com>
2024-10-12 13:26:18 +0530

    learning react

--- Learning React/src/App.jsx
+++ Learning React/src/App.jsx
@@ -1,17 +1,18 @@
 
 const App = () => {
 
-  const a = 'Arpan';
+  let user = 'Arpan';
 
-  const abc = () => {
-    console.log('hello');
-    
+  const changeUser = () => {
+    console.log(user);
+    user = 'Pratham'
+    console.log(user);
   }
 
   return (
     <div className="w-full h-screen bg-zinc-900 p-10">
-      <h1 className="text-3xl">Hello, {a}</h1>
-      <button className="bg-blue-400 px-5 py-2 rounded-md mt-5" onClick={abc}>Change user</button>
+      <h1 className="text-3xl">Hello, {user}</h1>
+      <button className="bg-blue-400 px-5 py-2 rounded-md mt-5" onClick={changeUser}>Change user</button>
     </div>
   )
 }
