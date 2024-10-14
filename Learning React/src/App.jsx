555e7f9f159d460ba18c034fa4979a575272db83
ArpanSankesh <arpansankesh830@gmail.com>
2024-10-14 10:48:34 +0530

    learning react

--- Learning React/src/App.jsx
+++ Learning React/src/App.jsx
@@ -1,18 +1,15 @@
+import { useState } from "react";
 
 const App = () => {
 
   let user = 'Arpan';
 
-  const changeUser = () => {
-    console.log(user);
-    user = 'Pratham'
-    console.log(user);
-  }
+  useState
 
   return (
     <div className="w-full h-screen bg-zinc-900 p-10">
       <h1 className="text-3xl">Hello, {user}</h1>
-      <button className="bg-blue-400 px-5 py-2 rounded-md mt-5" onClick={changeUser}>Change user</button>
+      <button className="bg-blue-400 px-5 py-2 rounded-md mt-5">Change user</button>
     </div>
   )
 }
