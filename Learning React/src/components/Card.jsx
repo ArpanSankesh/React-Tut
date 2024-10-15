a19d82c1bcedb6509488069743d8b393eba27ac4
ArpanSankesh <arpansankesh830@gmail.com>
2024-10-15 17:04:52 +0530

    Props

--- Learning React/src/components/Card.jsx
+++ Learning React/src/components/Card.jsx
@@ -1,15 +1,13 @@
-
-const Card = ({name, age, profession, city, img}) => {
+const Card = ({ name, age, profession, city, img }) => {
   return (
     <div className="w-80 h-96 bg-zinc-800 rounded-lg text-slate-100 p-5">
-            <img src={img} alt=""  className="w-full h-52 rounded-lg"/>
-            <h2 className="mt-3">{name}</h2>
-            <h2 className="mt-3">{age}</h2>
-            <h2 className="mt-3">{profession}</h2>
-            <h2 className="mt-3">{city}</h2>
-
+      <img src={img} alt="" className="w-full h-52 rounded-lg" />
+      <h2 className="mt-3">{name}</h2>
+      <h2 className="mt-3">{age}</h2>
+      <h2 className="mt-3">{profession}</h2>
+      <h2 className="mt-3">{city}</h2>
     </div>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
