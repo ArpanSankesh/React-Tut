f96a037f384ee6fef1f06d815ab7ee7bc72bf5ea
ArpanSankesh <arpansankesh830@gmail.com>
2024-10-15 11:45:58 +0530

    React

--- Learning React/src/App.jsx
+++ Learning React/src/App.jsx
@@ -1,25 +1,11 @@
-import { useState } from "react";
-
+import Nav from "./components/Nav"
 function App() {
-
- const [username, setUsername] = useState('')
-
- const submitHandler = (e) =>{
-  e.preventDefault()
-  console.log(username);
-  setUsername('')
- }
-
   return (
-    <div className="w-full h-screen bg-zinc-900 flex justify-center items-center">
-      <form className=" flex justify-center items-center flex-col "onSubmit={(e) => {submitHandler(e)}} >
-        <input
-        value={username} 
-        onChange={(e) => {setUsername(e.target.value)}}
-        type="text" placeholder="Name" className="py-3 px-5 rounded-lg m-2 text-xl outline-none"/>
-        <button  className="py-3 px-12 rounded-lg m-2 bg-blue-400 text-semibold text-xl">Submit</button>
-      </form>
+    <>
+    <div className=" w-full h-screen bg-zinc-900 text-slate-100">
+    <Nav/>
     </div>
+    </>
   )
 }
 
--- Learning React/src/components/nav.jsx
+++ Learning React/src/components/nav.jsx
@@ -1,6 +1,15 @@
+
 const nav = () => {
   return (
-    <div>nav</div>
+    <nav className="flex items-center justify-between p-10">
+        <h3 className="text-2xl">Logo</h3>
+    <ul className="flex items-center justify-center gap-11 text-xl">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Contact</a></li>
+    </ul>
+</nav>
   )
 }
 
