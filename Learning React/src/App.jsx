21d5b8542f1cc7fad1fb261cff660e4d7b6ab87a
ArpanSankesh <arpansankesh830@gmail.com>
2024-10-15 10:20:00 +0530

    React

--- Learning React/src/App.jsx
+++ Learning React/src/App.jsx
@@ -2,14 +2,22 @@ import { useState } from "react";
 
 const App = () => {
 
-  let user = 'Arpan';
+  // let a = 'Arpan';
 
-  useState
+  const [num , setNum] = useState(0)
+
+  const changeA = () =>{
+    setNum(num + 1)
+  }
+  const changeM = () =>{
+    setNum(num - 1)
+  }
 
   return (
     <div className="w-full h-screen bg-zinc-900 p-10">
-      <h1 className="text-3xl">Hello, {user}</h1>
-      <button className="bg-blue-400 px-5 py-2 rounded-md mt-5">Change user</button>
+      <h1 className="text-3xl">Number is : {num}</h1>
+      <button className="bg-blue-400 px-5 py-2 rounded-md mt-5 mr-2" onClick={changeA}>Increment</button>
+      <button className="bg-blue-400 px-5 py-2 rounded-md mt-5 " onClick={changeM}>Decrement</button>
     </div>
   )
 }
