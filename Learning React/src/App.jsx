015806ef275f9d4fb02fcdf06680a06306828889
ArpanSankesh <arpansankesh830@gmail.com>
2024-10-15 20:44:54 +0530

    Api

--- Learning React/src/App.jsx
+++ Learning React/src/App.jsx
@@ -1,58 +1,37 @@
-import Card from "./components/Card";
+import axios from 'axios'
+import { useEffect, useState } from 'react';
 
 const App = () => {
-  const users = [
-    {
-      name: "Alice Johnson",
-      age: 28,
-      city: "New York",
-      profession: "Software Engineer",
-      profile_photo: "https://example.com/profiles/alice_johnson.jpg",
-    },
-    {
-      name: "Michael Smith",
-      age: 35,
-      city: "San Francisco",
-      profession: "Designer",
-      profile_photo: "https://example.com/profiles/michael_smith.jpg",
-    },
-    {
-      name: "Sophia Williams",
-      age: 26,
-      city: "Chicago",
-      profession: "Software Engineer",
-      profile_photo: "https://example.com/profiles/sophia_williams.jpg",
-    },
-    {
-      name: "David Brown",
-      age: 31,
-      city: "Los Angeles",
-      profession: "Designer",
-      profile_photo: "https://example.com/profiles/david_brown.jpg",
-    },
-    {
-      name: "Emily Davis",
-      age: 29,
-      city: "Seattle",
-      profession: "Software Engineer",
-      profile_photo: "https://example.com/profiles/emily_davis.jpg",
-    },
-  ];
+
+  const [data, setData] = useState([])
+   
+  const getData = async () => {
+    const res = await axios.get('https://picsum.photos/v2/list')
+    setData(res.data);
+    console.log(data);
+  };
+
+  useEffect(() => {
+    getData()
+  }, [])
+  
 
   return (
-    <div className="w-full h-screen bg-zinc-900 p-20 flex gap-10">
-      {users.map((elem, idx) => {
-        return (
-          <Card
-            key={idx}
-            name={elem.name}
-            age={elem.age}
-            city={elem.city}
-            profession={elem.profession}
-            img={elem.profile_photo}
-          />
-        );
-      })}
+    <div className="w-full min-h-screen bg-zinc-900 p-10">
+      <button
+        onClick={getData}
+        className="bg-blue-400 px-8 py-4 rounded-lg text-slate-100 text-2xl focus:ring-4 focus:outline-none focus:ring-gray-100"
+      >
+        Get Data
+      </button>
+      <div className='mt-10 p-5 rounded flex flex-wrap gap-8'>
+        {data.map( (elem, idx) => {
+          return <div key={idx} className="bg-zinc-800 flex justify-between w-72 p-2 rounded-md mb-3 flex-col">
+            <img src={elem.download_url} alt="" />
+            <h1 className='text-slate-100 mt-2'>{elem.author}</h1>
+          </div>
+        })}
+      </div>
     </div>
   );
 };
