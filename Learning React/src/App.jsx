eab0e0533e16678037d64be58d08bb3c17b8b2f1
ArpanSankesh <arpansankesh830@gmail.com>
2024-10-19 11:18:57 +0530

    Context Api

--- Learning React/src/App.jsx
+++ Learning React/src/App.jsx
@@ -1,18 +1,15 @@
-import { useContext } from "react"
-import { DataContext } from "./context/UserContext"
+import { useContext } from "react";
+import { DataContext } from "./context/UserContext";
 
 const App = () => {
-
-const data = useContext(DataContext)
-console.log(data);
-
+  const data = useContext(DataContext);
+  console.log(data);
 
   return (
     <div className="bg-zinc-900 w-full h-screen p-10">
-  
-  <h1 className="text-white">This is App</h1>
+      <h1 className="text-white">This is App</h1>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
