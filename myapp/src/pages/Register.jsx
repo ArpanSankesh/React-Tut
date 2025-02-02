e3d1b483d907925199f64e9e7f4e8ac5ecf6129a
ArpanSankesh <arpansankesh830@gmail.com>
2025-02-02 19:57:21 +0530

    useEffect is a React hook that allows you to perform side effects (like data fetching or DOM manipulation) in functional components after they render

--- myapp/src/pages/Register.jsx
+++ myapp/src/pages/Register.jsx
@@ -1,21 +1,46 @@
+// /* eslint-disable react-hooks/rules-of-hooks */
+import { useEffect } from "react";
 import { useState } from "react";
 
 const Register = () => {
-
   function handelRegister(e) {
     e.preventDefault();
     console.log("hello");
   }
 
-  const [email, setemail] = useState('');
-  console.log(email);
-  
+  const handelCount = (e) => {
+    e.preventDefault();
+    setCount(count + 1);
+  };
+
+  const [email, setemail] = useState("");
+  const [pass, setPass] = useState("");
+  const [count, setCount] = useState("");
+
+  useEffect(() => {
+    setCount(0);
+  }, []);
+
   return (
     <>
+      <h1>{count}</h1>
+      <button onClick={handelCount}>Counter</button>
       <form action="#">
         <div>
-          <input type="text" name="email"  placeholder="Email" value={email} onChange={(e) => setemail(e.target.value)} />
-          <input type="password" name="pass"  placeholder="password" />
+          <input
+            type="text"
+            name="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setemail(e.target.value)}
+          />
+          <input
+            type="password"
+            name="pass"
+            placeholder="password"
+            value={pass}
+            onChange={(e) => setPass(e.target.value)}
+          />
           <button onClick={handelRegister}>Submit</button>
         </div>
       </form>
