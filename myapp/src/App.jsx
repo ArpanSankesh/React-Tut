4bdde1f9fa59d49d6ef6db2a4bd4d18d59700347
ArpanSankesh <arpansankesh830@gmail.com>
2025-02-04 14:23:43 +0530

    Creating Routes

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,10 +1,19 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
+import Register from './pages/Register';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <div>HELLO WORLD</div>
+  },
+  {
+    path: '/register',
+    element: <Register/>
+  },
+  {
+    path: '/projects',
+    element: <div>PROJECTS</div>
   },
 ]);
 
