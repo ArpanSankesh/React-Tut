891d4b1f25a59fc54e1ea3b68a2c39a12af70ccf
ArpanSankesh <arpansankesh830@gmail.com>
2025-02-04 10:44:37 +0530

    update

--- myapp/src/pages/Register.jsx
+++ myapp/src/pages/Register.jsx
@@ -3,49 +3,21 @@ import { useEffect } from "react";
 import { useState } from "react";
 
 const Register = () => {
-  function handelRegister(e) {
-    e.preventDefault();
-    console.log("hello");
-  }
-
-  const handelCount = (e) => {
-    e.preventDefault();
-    setCount(count + 1);
-  };
-
-  const [email, setemail] = useState("");
-  const [pass, setPass] = useState("");
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    console.log(`Count : ${count}`);
-  }, [count]);
+  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([second])
 
   return (
-    <>
-      <h1>{count}</h1>
-      <button onClick={handelCount}>Counter</button>
-      <form action="#">
-        <div>
-          <input
-            type="text"
-            name="email"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setemail(e.target.value)}
-          />
-          <input
-            type="password"
-            name="pass"
-            placeholder="password"
-            value={pass}
-            onChange={(e) => setPass(e.target.value)}
-          />
-          <button onClick={handelRegister}>Submit</button>
-        </div>
-      </form>
-    </>
-  );
+    <div>
+      <div>
+        <h2>My Todos</h2>
+        {todos.map((todos, index) => {
+          return <p key={index}>{todos}</p>
+        })}
+      </div>
+      <hr />
+      
+    </div>
+  )
 };
 
 export default Register;
